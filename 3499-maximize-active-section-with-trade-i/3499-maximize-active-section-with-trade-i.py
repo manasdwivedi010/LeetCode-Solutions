fa092ae41203b39fa43d3fe1f304cd4202b17675
MANAS DWIVEDI <dubeymanas618@gmail.com>
2026-08-09 12:44:03 +0530

    Time: 259 ms (97.84%), Space: 21.7 MB (38.56%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxActiveSectionsAfterTrade(self, s: str) -> int:
+        
+        zeros = map(len, filter(None, s.split("1")))
+
+        
+        maxzeros = max(map(sum, pairwise(zeros)), default=0)
+
+        return s.count("1") + maxzeros
+        
