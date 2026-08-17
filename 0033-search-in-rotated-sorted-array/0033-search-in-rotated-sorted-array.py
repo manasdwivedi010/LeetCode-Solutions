4e09ee0f33ba9a0574c699fb2e1cf657301cdb4e
MANAS DWIVEDI <dubeymanas618@gmail.com>
2026-08-17 15:11:42 +0530

    Time: 0 ms (100%), Space: 12.7 MB (9.53%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -5,7 +5,8 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        left, right = 0, len(nums) - 1
+        left=0
+        right = len(nums) - 1
 
         while left <= right:
             mid = (left + right) // 2
