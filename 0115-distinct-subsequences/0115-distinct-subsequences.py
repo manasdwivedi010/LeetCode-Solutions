7f4928b1cb7f14cc7b772e1d11929314683af552
MANAS DWIVEDI <dubeymanas618@gmail.com>
2026-09-06 14:04:26 +0530

    Time: 190 ms (91.13%), Space: 19.6 MB (76.61%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numDistinct(self, s, t):
+        n = len(s)
+        m = len(t)
+        sub = [0] * m
+        pos = defaultdict(list)
+        for i in range(m): pos[t[i]].append(i)
+
+        for i in s:
+            for j in reversed(pos[i]):
+                if j == 0:
+                    sub[j] += 1
+                else:
+                    sub[j] += sub[j - 1]
+        return sub[-1]
+        
